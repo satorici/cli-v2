34d1481b0e31e93e8f10e6f7fc5e2e2b0ea36527
gusalecar <gc.git@outlook.com>
2024-10-31 20:55:12 +0000

    Added run --show-output support

--- src/satori_cli_v2/commands/run.py
+++ src/satori_cli_v2/commands/run.py
@@ -6,7 +6,7 @@ import rich_click as click
 
 from ..api import client
 from ..utils.arguments import source_arg
-from ..utils.console import wait_job_until_finished
+from ..utils.console import show_execution_output, wait_job_until_finished
 from ..utils.options import input_opt, region_filter_opt, sync_opt
 
 
@@ -16,13 +16,18 @@ from ..utils.options import input_opt, region_filter_opt, sync_opt
 @sync_opt
 @region_filter_opt
 @input_opt
+@click.option("--show-output", "-o", is_flag=True)
 def run(
     source: dict,
     region_filter: tuple[str],
     count: int,
     sync: bool,
+    show_output: bool,
     input: Optional[dict[str, list[str]]],
 ):
+    if show_output and count > 1:
+        print("WARNING: Only first execution output will be shown")
+
     body = {
         "type": "RUN",
         "parameters": input,
@@ -36,6 +41,12 @@ def run(
     if not res.is_success:
         sys.exit(1)
 
-    if sync:
-        run_id = res.json()["id"]
+    run_id = res.json()["id"]
+
+    if sync or show_output:
         wait_job_until_finished(run_id)
+
+    if show_output:
+        res = client.get("/executions", params={"job_id": run_id})
+        execution_id = res.json()["items"][0]["id"]
+        show_execution_output(execution_id)
