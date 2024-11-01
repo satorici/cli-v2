ce9724ec5309921128e21f010f124b5b1583c949
gusalecar <gc.git@outlook.com>
2024-11-01 12:38:37 +0000

    Set v2 api by default

--- src/satori_cli_v2/api.py
+++ src/satori_cli_v2/api.py
@@ -5,6 +5,6 @@ from httpx import Client
 from .auth import SatoriAuth
 
 client = Client(
-    base_url=os.environ.get("SATORI_ENDPOINT", "https://api.satori.ci"),
+    base_url=os.environ.get("SATORI_ENDPOINT", "https://api-v2.satori.ci"),
     auth=SatoriAuth(),
 )
