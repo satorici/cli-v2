9dfed698e4574adee7afc1e1778542efccf402c6
gusalecar <gc.git@outlook.com>
2025-02-04 13:01:28 -0300

    Make reports job id positional arg

--- src/satori_cli_v2/commands/report.py
+++ src/satori_cli_v2/commands/report.py
@@ -7,10 +7,10 @@ from ..utils.console import stdout
 
 
 @click.command()
+@click.argument("job-id", type=int, required=False)
 @click.option("--page", default=1)
 @click.option("--quantity", default=10)
-@click.option("--job-id", type=int)
-def reports(page: int, quantity: int, job_id: Optional[int]):
+def reports(job_id: Optional[int], page: int, quantity: int):
     params = {k: v for k, v in locals().items() if v is not None}
 
     res = client.get("/executions/reports", params=params)
