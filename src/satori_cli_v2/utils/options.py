3be342ede389865ee7689668fdddddefcc6b73c5
gusalecar <gc.git@outlook.com>
2025-02-20 18:08:34 -0300

    Add json opt

--- src/satori_cli_v2/utils/options.py
+++ src/satori_cli_v2/utils/options.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 
 import click
 
+from ..config import config
+
 
 def _input_callback(ctx, name, inputs: tuple[str]):
     if inputs:
@@ -20,6 +22,11 @@ def _env_callback(ctx, name, envs):
         return {k: v for k, v in envs}
 
 
+def _json_callback(ctx, name, json_):
+    config["json"] = json_
+    return json_
+
+
 input_opt = click.option(
     "--data", "-d", "input", multiple=True, callback=_input_callback
 )
@@ -31,3 +38,6 @@ sync_opt = click.option("--sync", "-s", is_flag=True, default=False)
 profile_opt = click.option("--profile", default="default")
 cpu_opt = click.option("--cpu", type=int)
 memory_opt = click.option("--memory", type=int)
+json_opt = click.option(
+    "--json", "json_", is_flag=True, default=False, callback=_json_callback
+)
