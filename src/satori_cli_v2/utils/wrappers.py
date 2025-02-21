e7c6739d4ca26489988b5267af42acf7d48cac57
gusalecar <gc.git@outlook.com>
2025-02-20 21:21:02 -0300

    Create has_json_output class decorator

--- src/satori_cli_v2/utils/wrappers.py
+++ src/satori_cli_v2/utils/wrappers.py
@@ -18,11 +18,25 @@ class Wrapper(Generic[T]):
         raise NotImplementedError
 
 
-class JobWrapper(Wrapper[dict]):
-    def __rich_console__(self, console, options):
+W = TypeVar("W", bound=Wrapper)
+
+
+def has_json_output(cls: type[W]):
+    orig = cls.__rich_console__
+
+    def __rich_console__(self: W, console: Console, options: ConsoleOptions):
         if config.get("json"):
             yield json.dumps(self.obj, indent=2)
-            return
+        else:
+            yield from orig(self, console, options)
 
+    cls.__rich_console__ = __rich_console__
+
+    return cls
+
+
+@has_json_output
+class JobWrapper(Wrapper[dict]):
+    def __rich_console__(self, console, options):
         yield f"Job id: {self.obj['id']}"
         yield f"Job type: {self.obj['type']}"
