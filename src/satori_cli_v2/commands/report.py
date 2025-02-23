e081a1b517c01ebd7053d234cafa00f8f63e7059
gusalecar <gc.git@outlook.com>
2025-02-23 01:19:22 -0300

    Add paged and execution list wrappers

--- src/satori_cli_v2/commands/report.py
+++ src/satori_cli_v2/commands/report.py
@@ -3,15 +3,18 @@ from typing import Optional
 import rich_click as click
 
 from ..api import client
+from ..utils import options as opts
 from ..utils.console import stdout
+from ..utils.wrappers import ExecutionListWrapper, PagedWrapper
 
 
 @click.command()
 @click.argument("job-id", type=int, required=False)
 @click.option("--page", default=1)
 @click.option("--quantity", default=10)
-def reports(job_id: Optional[int], page: int, quantity: int):
-    params = {k: v for k, v in locals().items() if v is not None}
+@opts.json_opt
+def reports(job_id: Optional[int], page: int, quantity: int, **kwargs):
+    params = {k: v for k, v in locals().items() if v is not None and k != "kwargs"}
 
     res = client.get("/executions", params=params)
-    stdout.print_json(res.text)
+    stdout.print(PagedWrapper(res.json(), page, quantity, ExecutionListWrapper))
--- src/satori_cli_v2/utils/wrappers.py
+++ src/satori_cli_v2/utils/wrappers.py
@@ -1,7 +1,9 @@
 import json
+from math import floor
 from typing import Generic, TypeVar
 
 from rich.console import Console, ConsoleOptions, RenderResult
+from rich.table import Table
 
 from ..config import config
 
@@ -49,3 +51,44 @@ class ExecutionWrapper(Wrapper[dict]):
         yield f"Status: {self.obj['status']}"
         yield JobWrapper(self.obj["job"])
         yield "Report: " + json.dumps(self.obj["data"]["report"], indent=2)
+
+
+@has_json_output
+class PagedWrapper(Wrapper[dict]):
+    def __init__(self, obj, page: int, quantity: int, items_wrapper: type[Wrapper]):
+        self._wrapper = items_wrapper
+        self.page = page
+        self.quantity = quantity
+        super().__init__(obj)
+
+    def __rich_console__(self, console, options):
+        yield self._wrapper(self.obj["items"])
+        yield f"Page {self.page} of {floor(self.obj['total'] / self.quantity)}"
+
+
+class ExecutionListWrapper(Wrapper[dict]):
+    def __rich_console__(self, console, options):
+        table = Table()
+        table.add_column("Id")
+        table.add_column("Status")
+        table.add_column("Visibility")
+        table.add_column("Job type")
+        table.add_column("Job id")
+        table.add_column("Result")
+
+        for execution in self.obj:
+            if report := execution["data"].get("report"):
+                result = "FAIL" if report["fails"] else "PASS"
+            else:
+                result = "N/A"
+
+            table.add_row(
+                str(execution["id"]),
+                execution["status"],
+                execution["visibility"],
+                execution["job"]["type"],
+                str(execution["job"]["id"]),
+                result,
+            )
+
+        yield table
