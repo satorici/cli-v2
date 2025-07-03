40766af61c4a0cd99463a7a5a43bee11c305c0cb
gusalecar <gc.git@outlook.com>
2025-07-03 10:07:21 -0300

    Add stop all command

--- src/satori_cli/commands/stop.py
+++ src/satori_cli/commands/stop.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor
+
 import rich_click as click
 
 from ..api import client
@@ -16,3 +18,24 @@ def stop():
 def run(run_id: int):
     client.patch(f"/jobs/runs/{run_id}", json={"status": "CANCELED"})
     stdout.print("Run stopped")
+
+
+@stop.command()
+def all():
+    res = client.get("/jobs", params={"type": "RUN", "quantity": 100})
+
+    last_runs = res.json()
+
+    run_ids = [
+        run["id"]
+        for run in last_runs["items"]
+        if run["status"] in ("QUEUED", "RUNNING")
+    ]
+
+    with ThreadPoolExecutor() as executor:
+        for run_id in run_ids:
+            executor.submit(
+                client.patch, f"/jobs/runs/{run_id}", json={"status": "CANCELED"}
+            )
+
+    stdout.print("Runs stopped")
