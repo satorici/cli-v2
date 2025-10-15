7bca2992005ee70961d75fed6fb4bec61115d312
gusalecar <gc.git@outlook.com>
2025-10-14 22:31:02 -0300

    Show last 5 executions in job

--- src/satori_cli/commands/job.py
+++ src/satori_cli/commands/job.py
@@ -2,7 +2,7 @@ import rich_click as click
 
 from ..api import client
 from ..utils.console import stdout
-from ..utils.wrappers import JobWrapper
+from ..utils.wrappers import ExecutionListWrapper, JobWrapper
 
 job_id_arg = click.argument("job-id", type=int)
 
@@ -12,3 +12,6 @@ job_id_arg = click.argument("job-id", type=int)
 def job(job_id: int):
     res = client.get(f"/jobs/{job_id}")
     stdout.print(JobWrapper(res.json()))
+    res = client.get("/executions", params={"job_id": job_id, "quantity": 5})
+    stdout.print("[b]Last 5 executions[/b]")
+    stdout.print(ExecutionListWrapper(res.json()["items"]))
