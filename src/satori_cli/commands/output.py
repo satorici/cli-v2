3a837352f7bc38064c740cf8a5fab04985b9cf35
gusalecar <gc.git@outlook.com>
2026-05-13 21:29:30 -0300

    Add option to get raw output

--- src/satori_cli/commands/output.py
+++ src/satori_cli/commands/output.py
@@ -1,5 +1,8 @@
+import sys
+
 import rich_click as click
 
+from ..api import client
 from ..utils.console import show_execution_output
 
 execution_id_arg = click.argument("execution-id", type=int)
@@ -7,5 +10,18 @@ execution_id_arg = click.argument("execution-id", type=int)
 
 @click.command()
 @execution_id_arg
-def output(execution_id: int):
+@click.option("--raw", help="Pipe encoded results to stdout", is_flag=True)
+def output(execution_id: int, raw: bool):
+    if raw:
+        with client.stream(
+            "GET",
+            f"/executions/{execution_id}/output",
+            follow_redirects=True,
+            timeout=None,
+        ) as stream:
+            for b in stream.iter_bytes():
+                sys.stdout.buffer.write(b)
+
+        return
+
     show_execution_output(execution_id)
