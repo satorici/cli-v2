7d0db477a486ee7d8e4f24a66fc96355a221a236
marian <marianocarrazana@gmail.com.com>
2026-09-03 22:03:03 -0300

    Add 'advisory' subcommand for external issues

--- src/satori_cli/commands/issue.py
+++ src/satori_cli/commands/issue.py
@@ -3,6 +3,8 @@ import rich_click as click
 from ..api import client
 from ..utils import options as opts
 from ..utils.console import stdout
+from ..utils.format import is_json_output
+from ..utils.groups import IdGroup
 from ..utils.wrappers import IssueListWrapper, IssueWrapper, PagedWrapper
 
 
@@ -28,9 +30,26 @@ def issues(execution_id: int, page: int, quantity: int, **kwargs):
     list_issues(execution_id, page, quantity)
 
 
-@click.command("issue")
-@click.argument("finding-id", type=int)
+@click.group(cls=IdGroup, invoke_without_command=True)
 @opts.json_opt
-def issue(finding_id: int, **kwargs):
-    res = client.get(f"/findings/{finding_id}")
-    stdout.print(IssueWrapper(res.json()))
+@click.pass_context
+def issue(ctx, **kwargs):
+    if ctx.invoked_subcommand is None:
+        if ctx.obj is None:
+            raise click.UsageError("Missing argument 'FINDING-ID'.")
+        res = client.get(f"/findings/{ctx.obj}")
+        stdout.print(IssueWrapper(res.json()))
+
+
+@issue.command(name="advisory")
+@opts.json_opt
+@click.pass_obj
+def issue_advisory(finding_id: int, **kwargs):
+    if finding_id is None:
+        raise click.UsageError("Missing argument 'FINDING-ID'.")
+    res = client.post("/external_issues", json={"finding_id": finding_id})
+    data = res.json()
+    if is_json_output():
+        stdout.print_json(data)
+    else:
+        stdout.print(data.get("external_url") or data["external_id"])
